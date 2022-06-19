1d2abb74c64b7cb037d6e9ef9b8f0b8c53db8c12
GoldilocksZone <jmarulli@gmail.com>
2022-06-19 09:10:43 -0400

    Added functionality to check the amount of money tendered.

--- src/com/skilldistillery/makechange/MakeChange.java
+++ src/com/skilldistillery/makechange/MakeChange.java
@@ -8,16 +8,31 @@ public class MakeChange {
 	
 	
 	public static void main(String[] args) {
-		/* Print the header before anything else
-		 * to clearly show the user
-		 * which program they are using.
+		/*	Print the header before anything else
+		 *	to clearly show the user
+		 *	which program they are using.
 		 */
 		printHeader();
 		
-		// Declare variables and
-		// initialize with relevant method calls
-		double price = getPrice(),
-				moneyTendered = getMoneyTendered();
+		/*	Step 1.	getPrice()
+		 * 			Get the price of an item from the user.
+		 * 
+		 *	Step 2.	getMoneyTendered()
+		 * 			Prompt the user to enter the amount of
+		 * 			money tendered.
+		 * 
+		 *	Step 3.	checkAmount()
+		 * 			Evaluate the money tendered relative to the
+		 * 			price of the item, and present a message
+		 * 			if the customer provided too little money or
+		 * 			exact change.
+		 * 			-	If the customer provided too little money,
+		 * 				present options to re-enter money tendered
+		 * 				or cancel the transaction.
+		 * 			-	If the customer provided more than exact
+		 * 				change, output the change to be made.
+		 */
+		checkAmount(getPrice(), getMoneyTendered());
 		
 	}
 
@@ -33,18 +48,45 @@ public class MakeChange {
 	 * 			price of the item.
 	 */
 	public static double getPrice() {
-		System.out.print("Item price:     ");
+		System.out.print("Item price:     $");
 		return scanner.nextDouble();
 	}
 	
 	/* Prompt the user for the amount of money
 	 * tendered by the customer.
-	 * @return	a double containing the user-provided
+	 * @return	moneyTendered
+	 * 			a double containing the user-provided
 	 * 			amount of money tendered.
 	 */
 	public static double getMoneyTendered() {
-		System.out.print("Money Tendered: ");
+		/* Initialize local variable to store the
+		 * amount of money tendered.
+		 * This will be the return variable
+		 */
+		double moneyTendered = 0.0;
+		
+		System.out.print("Money Tendered: $");
 		return scanner.nextDouble();
+	}
+	
+	/* Check to see whether the money tendered
+	 * was sufficient to pay for the priced item.
+	 * Send one message if it is insufficient and
+	 * another if it exact change.
+	 */
+	public static void checkAmount(double price, double moneyTendered) {
+		if (price > moneyTendered) {
+			System.out.println("Payment Insufficient");
+			
+		} else if (price == moneyTendered){
+			System.out.println("Exact Change Provided");
+		} else {
+			displayChange();
+		}
+	}
+	
+	public static void displayChange() {
+		
 	}
 	
 }
